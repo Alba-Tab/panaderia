60b3539937757bcceeab39fa8ef89980ac7ab53c
Eude <eudeniaflores@gmail.com>
2024-10-11 01:44:16 -0400

    Create categoria.routes.js

---
+++ backend/routes/categoria.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { getCategorias, createCategoria, updateCategoria, deleteCategoria } = require('../controllers/categorias.controllers');
+const router = express.Router();
+
+router.get('/categorias', getCategorias);
+router.get('/categorias/:id', getCategoria);
+router.post('/categorias', createCategoria);
+router.put('/categorias/:id', updateCategoria);
+router.delete('/categorias/:id', deleteCategoria);
+
+module.exports = router;
